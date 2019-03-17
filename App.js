341f13bebd87893893d13c826334f4549797d824
Marko Durasinovic <marko.durasinovic@gmail.com>
2019-03-17 16:09:51 +0000

    moving some 'browser' functionality to the class Browser

--- App.js
+++ App.js
@@ -1,104 +1,67 @@
 const argv = require('yargs').argv;
-const PlayerLoader = require('./PlayerLoader');
-const puppeteer = require('puppeteer');
 const Browser = require('./Browser');
+const PlayerLoader = require('./PlayerLoader');
+
 const homePage = 'https://www.dugout-online.com/';
+const browser = new Browser();
 
-async function main(to) {
-    let browser = new Browser(homePage);
+async function main() {
     await browser.start(homePage);
-    let page = await browser.getPage();
 
-    await login(page, argv.username, argv.password);
-    await goTo(page, 'Club');
-    await goTo(page, 'Players');
-    await movePlayers(page, to);
+    await login(argv.username, argv.password);
+    await movePlayers(argv.to);
     
     browser.quit();
 };
 
-async function movePlayers(page, to) {
-    let playerLoader = new PlayerLoader();
-    let players = playerLoader.getPlayers(argv.path);
-    
-    if (to === 'youth') {
-        await movePlayersToYouth(page, players);
-    } else {
-        await movePlayersToSenior(page, players);
-    }
-};
-
-async function movePlayersToYouth(page, playersToTransfer) {
-    for (player of playersToTransfer) {
-        if (player === '\n') {
-            return;
-        }
-        await goTo(page, player);        
-        await clickLink(page, 'input[value="Move to youth"]');
-        await goTo(page, 'Players');
-    }    
-    console.log('All players moved to youth')
-};
-
-async function movePlayersToSenior(page, playersToTransfer) {
-    for (player of playersToTransfer) {
-        if (player === '\n') {
-            return;
-        }
-        await clickLink(page, 'div[id="youth2"]');
-        await goTo(page, player);
-        await clickLink(page, 'input[value="Move to 1st"]');
-        await goTo(page, 'Players')
-    }
-    console.log('All players moved to senior')
-};
-
-async function login(page, username, password) {
+async function login(username, password) {
     console.log('Loging in as', username);
 
-    await fillField(page, 'input[name=do_user]', username);
-    await fillField(page, 'input[name=do_pass]', password);
-    await clickLink(page, 'input[value=Login]');
-};
+    await browser.fillField('input[name=do_user]', username);
+    await browser.fillField('input[name=do_pass]', password);
+    await browser.clickLink('input[value=Login]');
+}
 
-async function fillField(page, fieldToSelect, fillValue) {
-    const field = await page.$(fieldToSelect);
-    await field.click();
-    await field.type(fillValue);
-};
-
-async function clickLink(page, fieldToSelect) {
-    const field = await page.$(fieldToSelect);
-    await field.click();
-    await page.waitForNavigation();
-};
-
-async function goTo(page, pageName) {
-    console.log('Going to', pageName, 'page');
-
-    let link = getPage(page, pageName);
-    link.then((res) => {
-        if (res === null) {
-            console.log('Page', pageName, 'could not be found');
-            return false;
-        }
-        res.click();
-    });
-    await page.waitForNavigation();
-
-    return true;
-};
-
-async function getPage(page, pageName) {
-    const links = await page.$$('a');
-    for (var i = 0; i < links.length; i++) {
-        let valueHandle = await links[i].getProperty('innerText');
-        let linkText = await valueHandle.jsonValue();
-        if (linkText === pageName) {
-            return links[i];
-        }
+async function movePlayers(to) {
+    const playerLoader = new PlayerLoader();
+    const players = playerLoader.getPlayers(argv.path);
+    
+    await goToPlayersPage();
+    if (to === 'youth') {
+        await movePlayersToYouth(players);
+    } else {
+        await movePlayersToSenior(players);
     }
-    return null;
-};
+}
 
-main(argv.to);
+async function goToPlayersPage() {
+    await browser.goToPage('Club');
+    await browser.goToPage('Players');
+}
+
+async function movePlayersToYouth(playersToTransfer) {
+    for (player of playersToTransfer) {
+        if (player === '\n') {
+            console.log('All players moved to youth')
+            return;
+        }
+        await browser.goToPage(player);        
+        await browser.clickLink('input[value="Move to youth"]');
+        await browser.goToPage('Players');
+    }    
+}
+
+async function movePlayersToSenior(playersToTransfer) {
+    for (player of playersToTransfer) {
+        if (player === '\n') {
+            console.log('All players moved to senior');
+            return;
+        }
+        await browser.clickLink('div[id="youth2"]');
+        await browser.goToPage(player);
+        await browser.clickLink('input[value="Move to 1st"]');
+        await browser.goToPage('Players');
+    }
+}
+
+main();
--- Browser.js
+++ Browser.js
@@ -1,10 +1,6 @@
 const puppeteer = require('puppeteer');
 
 module.exports = class Browser {
-    constructor(homePage) {
-        this.start(homePage);
-    }
-
     async start(homePage) {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
@@ -14,19 +10,48 @@ module.exports = class Browser {
         this.page = page;
     }
 
+    async goToPage(pageName) {
+        console.log('Going to', pageName, 'page');
+
+        const link = this.getPage(pageName);
+        link.then((res) => {
+            if (res === null) {
+                console.log('Page', pageName, 'could not be found');
+                process.exit(1);
+            }
+            res.click();
+        });
+        await this.page.waitForNavigation();
+    }
+
+    async getPage(pageName) {
+        const links = await this.page.$$('a');
+        for (var i = 0; i < links.length; i++) {
+            const valueHandle = await links[i].getProperty('innerText');
+            const linkText = await valueHandle.jsonValue();
+            if (linkText === pageName) {
+                return links[i];
+            }
+        }
+        return null;
+    }
+
+    async clickLink(fieldToClick) {
+        const field = await this.page.$(fieldToClick);
+        await field.click();
+        await this.page.waitForNavigation();
+    }
+
+    async fillField(fieldToFill, fillValue) {
+        const field = await this.page.$(fieldToFill);
+        await field.click();
+        await field.type(fillValue);
+    }
+
     quit() {
         console.log('Closing the browser!');
 
         this.browser.close();
         process.exit(0);
-    }
-
-    getPage() {
-        if (this.page === undefined) {
-            console.log('Critical error: Browser failed to find page');
-            process.exit(1);
-        }
-
-        return this.page;
     }
 }
